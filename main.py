d065bce1415da8bd883431df497884012d0c6425
Natnael Zemene <natnaelzemene21@gmail.com>
2026-03-01 14:31:04 +0300

    storing and creating arrays

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -33,24 +34,32 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    
+#     rating: Optional[int] = None
+
+my_posts = [ {"title ": "title of post 1", "content": "content of post 1", "id": 1}, {"title ": "favorite foods", "content": "I like pizza", "id": 2} ]
+
+def find_post(id):
+     for p in my_posts:
+          if p["id"] == id:
+               return p
+
+
+     
 
 @app.post("/posts")    
 def create_posts(post: Post):
-    new_post = post.dict()
-    new_post["id"] = len(posts) + 1
-    posts.append(new_post)
-
-    print(f"""
-    Id: {new_post['id']}
-    Title: {new_post['title']}
-    Content: {new_post['content']}
-    Is Published: {new_post['published']}
-    """)
-
-    return new_post
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 10000000)
+    posts.append(post_dict)
+    return {"data": post_dict}
 
 
-# @app.get('/post/{id}')
-# def get_posts_id(post_id : Post):
+@app.get('/posts/{id}')
+def get_post(id: int):
+     print(id)
+     post = find_post(id)
+     print(post)
+     return {"post_detail": post}
      
+
+
